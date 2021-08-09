f378b4cff861f3f44b8be001344ce11b9f49733c
Jim Shaw <jimxshaw@gmail.com>
2021-08-08 19:49:34 -0500

    Initialize nav controller with container view inside main activity.

--- app/src/main/java/com/example/coinsanddie/MainActivity.kt
+++ app/src/main/java/com/example/coinsanddie/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.coinsanddie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize the nav controller with a container view that it
+        // will use to display all the other views.
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.containerFragment) as NavHostFragment
+
+        this.navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setupWithNavController(this.navController)
     }
 }
