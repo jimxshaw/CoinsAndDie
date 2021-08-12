58bbef36ff6ce1f6d4cfd50a59bedfd60de8672a
Jim Shaw <jimxshaw@gmail.com>
2021-08-11 22:14:26 -0500

    Update GameFragment class to inflate view with data binding.

--- app/src/main/java/com/example/coinsanddie/fragments/GameFragment.kt
+++ app/src/main/java/com/example/coinsanddie/fragments/GameFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.coinsanddie.R
+import com.example.coinsanddie.databinding.FragmentGameBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,9 +34,10 @@ class GameFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    ): View {
+        val binding = FragmentGameBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     companion object {
